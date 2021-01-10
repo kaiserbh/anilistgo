e54c7fc582a1e8bb1d7c0c6dc7312822d9e0d959
KaiserBh <rezamuhamad1@gmail.com>
2021-01-10 20:30:18 +1100

    refactored and added character query

---
+++ anilist/character.go
@@ -0,0 +1,84 @@
+package anilist
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Character Query
+type Character struct {
+	ID          int    `json:"id"`
+	Name        Name   `json:"name"`
+	Image       Image  `json:"image"`
+	Description string `json:"description"`
+	IsFavourite bool   `json:"isFavourite"`
+	SiteURL     string `json:"siteUrl"`
+	Favourites  int    `json:"favourites"`
+	ModNotes    string `json:"modNotes"`
+}
+
+const characterQuery = `id,
+					name {
+						first,
+						last,
+						full,
+						native,
+					},
+					image {
+						large,
+						medium,
+					},
+					description,
+					isFavourite,
+					siteUrl,
+					favourites,
+					modNotes,
+				`
+
+// FilterCharacterByName filters the characters from aniList by search query or name of the character.
+func (c *Character) FilterCharacterByName(search string) {
+	jsonData := map[string]string{
+		"query": fmt.Sprintf(`
+		{ 
+			Character(search: "%v") {
+				%s
+			  }
+		}
+	`, search, characterQuery),
+	}
+
+	jsonValue, err := json.Marshal(jsonData)
+	if err != nil {
+		panic(err)
+	}
+
+	cleanData := CleanCharacterJSON(PostRequest(jsonValue))
+	if err := json.Unmarshal(cleanData, &c); err != nil {
+		panic(err)
+	}
+
+}
+
+// FilterCharacterID search the character by it's ID
+func (c *Character) FilterCharacterID(ID int) {
+	jsonData := map[string]string{
+		"query": fmt.Sprintf(`
+		{ 
+			Character(id: %v) {
+				%s
+			  }
+		}
+	`, ID, characterQuery),
+	}
+
+	jsonValue, err := json.Marshal(jsonData)
+	if err != nil {
+		panic(err)
+	}
+
+	cleanData := CleanCharacterJSON(PostRequest(jsonValue))
+	if err := json.Unmarshal(cleanData, &c); err != nil {
+		panic(err)
+	}
+
+}
--- anilist/media.go
+++ anilist/media.go
@@ -1,12 +1,8 @@
 package anilist
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
 )
 
 // Media object to store the json from Anilist
@@ -576,79 +572,4 @@ func (m *Media) FilterMangaByID(id int) {
 		panic(err)
 	}
 
-}
-
-// PostRequest sends POST request that takes []byte as parameter then returns back httml Body as []byte
-func PostRequest(jsonValue []byte) []byte {
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	// Check if the respone is 200 if not then media is not found or server might be down.
-	if resp.StatusCode != 200 {
-		fmt.Println("Media Not Found got Error with statusCode: ", resp.StatusCode)
-	}
-
-	if resp.Body != nil {
-		defer resp.Body.Close()
-	}
-
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	return data
-}
-
-// CleanJSON cleans the html body by removing unnecessary html tags using regex.
-// That then returns []byte data and takes []byte as parameter
-func cleanMediaJSON(str []byte) []byte {
-	re := regexp.MustCompile(`(?m){"data":{"Media":`)
-	substitution := ""
-	firstItr := re.ReplaceAll(str, []byte(substitution))
-	// clean the last two ]] from the string
-	reCleanReminders := regexp.MustCompile(`(?m)}}$`)
-	cleanJSON := reCleanReminders.ReplaceAll(firstItr, []byte(substitution))
-
-	// Clean the html tags in description
-	reCleanHTMLDescription := regexp.MustCompile(`(?m)<br>\\n`)
-	cleanDescription := reCleanHTMLDescription.ReplaceAll(cleanJSON, []byte(substitution))
-
-	// Clean the i tags
-	cleanTags := regexp.MustCompile(`(?m)<i>`)
-	cleanTagsI := regexp.MustCompile(`(?m)<\\/i>`)
-
-	secondItr := cleanTags.ReplaceAll(cleanDescription, []byte(substitution))
-	lastItr := cleanTagsI.ReplaceAll(secondItr, []byte(substitution))
-
-	return lastItr
-}
-
-// CleanPageJSON cleans the hmtl body by removing data and }}
-func CleanPageJSON(str []byte) []byte {
-	re := regexp.MustCompile(`(?m){"data":{"Page":|}}$`)
-	substitution := ""
-	firstItr := re.ReplaceAll(str, []byte(substitution))
-
-	return firstItr
-}
-
-// CleanMediaTrendPageJSON cleans the hmtl body by removing data and }}
-func CleanMediaTrendPageJSON(str []byte) []byte {
-	re := regexp.MustCompile(`(?m){"data":{"MediaTrend":|}}$`)
-	substitution := ""
-	firstItr := re.ReplaceAll(str, []byte(substitution))
-
-	return firstItr
-}
-
-// CleanStaffJSON cleans the hmtl body by removing data and }}
-func CleanStaffJSON(str []byte) []byte {
-	re := regexp.MustCompile(`(?m){"data":{"Staff":|}}$`)
-	substitution := ""
-	firstItr := re.ReplaceAll(str, []byte(substitution))
-
-	return firstItr
 }
